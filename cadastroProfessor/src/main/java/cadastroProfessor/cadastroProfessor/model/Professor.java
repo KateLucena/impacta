31b16abf680dd42d845786be853a8d8775823937
KateLucena <kate.lucena2011@gmail.com>
2023-01-17 22:08:17 -0300

    Conexão BD

---
+++ cadastroProfessor/src/main/java/cadastroProfessor/cadastroProfessor/model/Professor.java
@@ -0,0 +1,53 @@
+package cadastroProfessor.cadastroProfessor.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Professor {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
+	private String nome;
+	
+	private int idade;
+	
+	private String matricula;
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
