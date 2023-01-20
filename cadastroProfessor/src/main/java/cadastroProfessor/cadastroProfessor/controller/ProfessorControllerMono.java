62c438271938ade0112bca7e1f88a49fcca31138
KateLucena <kate.lucena2011@gmail.com>
2023-01-19 23:12:15 -0300

    Adjust object name

--- cadastroProfessor/src/main/java/cadastroProfessor/cadastroProfessor/controller/ProfessorControllerMono.java
+++ cadastroProfessor/src/main/java/cadastroProfessor/cadastroProfessor/controller/ProfessorControllerMono.java
@@ -29,7 +29,7 @@ public class ProfessorControllerMono {
 
 
 
-		modelAndViewProfessor.addObject("professor", professores);
+		modelAndViewProfessor.addObject("professores", professores);
 
 
 		return modelAndViewProfessor;
